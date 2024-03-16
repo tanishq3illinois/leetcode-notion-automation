28ab792caf0df3db0b4f2ed2b5a577f80b097785
= <=>
2024-03-16 18:56:57 +0400

    Updating Notion table with new submissions completed

--- server/leetcode.ts
+++ server/leetcode.ts
@@ -1,18 +1,25 @@
-import { LeetCode, Credential, RecentSubmission } from "leetcode-query";
+import { LeetCode, Credential, Submission } from "leetcode-query";
 
-let prev_submissions: RecentSubmission[] = [];
+let prev_submissions: Submission[] = [];
 
 export const initLeetCode = async () => {
   const credential = new Credential();
+  credential.init(process.env.LEETCODE_SESSION_COOKIE ?? "");
   return new LeetCode(credential);
 };
 
 export const getRecentSubmissions = async (leetcode: LeetCode) => {
-  return await leetcode.recent_submissions("taimurshaikh");
+  return await leetcode.submissions({ limit: 4000, offset: 0 });
 };
 
-export const getNewSubmissions = (submissions: RecentSubmission[]) => {
-  let new_submissions: RecentSubmission[] = submissions;
+/**
+ * The function `getNewSubmissions` filters out submissions that have already been processed and
+ * returns only the new submissions.
+ * @param {Submission[]} submissions - The `submissions` parameter is an array of `Submission` objects.
+ * @returns an array of new submissions.
+ */
+export const getNewSubmissions = (submissions: Submission[]) => {
+  let new_submissions: Submission[] = submissions;
 
   if (prev_submissions.length !== 0) {
     new_submissions = submissions.filter(
@@ -23,22 +30,31 @@ export const getNewSubmissions = (submissions: RecentSubmission[]) => {
   return new_submissions;
 };
 
-export const getAcceptedSubmissions = (submissions: RecentSubmission[]) => {
+export const getAcceptedSubmissions = (submissions: Submission[]) => {
   return submissions.filter((s) => s.statusDisplay === "Accepted");
 };
 
+/**
+ * The function removes duplicate submissions from an array based on the titleSlug and keeps the one
+ * with the latest timestamp.
+ * @param {Submission[]} submissions - An array of objects representing submissions. Each submission
+ * object has the following properties:
+ * @returns The function `removeDuplicateSubmissions` returns an array of `Submission` objects.
+ */
 export const removeDuplicateSubmissions = (
-  submissions: RecentSubmission[]
-): RecentSubmission[] => {
-  const uniqueSubmissions: RecentSubmission[] = [];
+  submissions: Submission[]
+): Submission[] => {
+  const uniqueSubmissions: Submission[] = [];
 
   submissions.forEach((submission) => {
     const existingSubmission = uniqueSubmissions.find(
       (s) => s.titleSlug === submission.titleSlug
     );
+    // If the submission does not exist in the array or if the submission has a more recent timestamp
+    // (Note: this probably won't happen as the submissions are sorted by timestamp)
     if (
       !existingSubmission ||
-      parseInt(submission.timestamp) > parseInt(existingSubmission.timestamp)
+      submission.timestamp > existingSubmission.timestamp
     ) {
       if (existingSubmission) {
         uniqueSubmissions.splice(
@@ -53,9 +69,19 @@ export const removeDuplicateSubmissions = (
   return uniqueSubmissions;
 };
 
+/**
+ * The function "getProblemFromSubmission" takes a LeetCode object and a Submission object as
+ * parameters, and returns the problem associated with the submission.
+ * @param {LeetCode} leetcode - The `leetcode` parameter is an instance of the `LeetCode` class, which
+ * is used to interact with the LeetCode API and perform operations related to problems, submissions,
+ * etc.
+ * @param {Submission} submission - The `submission` parameter is an object that represents a
+ * submission made by the user on LeetCode
+ * @returns a promise that resolves to the problem object.
+ */
 export const getProblemFromSubmission = async (
   leetcode: LeetCode,
-  submission: RecentSubmission
+  submission: Submission
 ) => {
   const problem = await leetcode.problem(submission.titleSlug);
   return problem;
--- server/notion.ts
+++ server/notion.ts
@@ -1,13 +1,6 @@
 import { Client } from "@notionhq/client";
-import { LeetCode, RecentSubmission, Submission } from "leetcode-query";
+import { LeetCode, Submission } from "leetcode-query";
 import { getProblemFromSubmission } from "./leetcode";
-import { get } from "http";
-
-const difficulty_map: { [key: string]: string } = {
-  Easy: "green",
-  Medium: "orange",
-  Hard: "red",
-};
 
 export const initNotion = () => {
   const conn = new Client({
@@ -16,76 +9,139 @@ export const initNotion = () => {
   return conn;
 };
 
+/**
+ * Updates a Notion database with information from a
+ * LeetCode submission.
+ * @param {Client} notion - The `notion` parameter is an instance of the Notion client, which is used
+ * to interact with the Notion API and perform operations on Notion pages.
+ * @param {string} notion_database_id - The `notion_database_id` parameter is the ID of the Notion
+ * database where you want to update the submission. It is a string value that uniquely identifies the
+ * database in Notion.
+ * @param {LeetCode} lc - The `lc` parameter is an instance of the `LeetCode` class, which is used to
+ * interact with the LeetCode API and retrieve information about problems and submissions.
+ * @param {Submission} submission - The `submission` parameter is an object that represents a
+ * submission made by a user on a coding platform. It contains information such as the submission ID,
+ * timestamp, and code.
+ */
 const updateDatabaseWithSubmission = async (
   notion: Client,
   notion_database_id: string,
   lc: LeetCode,
-  submission: RecentSubmission
+  submission: Submission
 ) => {
   const problem = await getProblemFromSubmission(lc, submission);
+  const submission_detail = await lc.submission(submission.id);
 
-  const response = await notion.pages.create({
-    parent: {
-      database_id: notion_database_id ?? "",
-    },
-    properties: {
-      "Problem Number": {
-        type: "number",
-        number: parseInt(problem.questionFrontendId),
-      },
-      "Problem Name": {
-        type: "title",
-        title: [
+  const createPage = async () => {
+    try {
+      const response = await notion.pages.create({
+        parent: {
+          database_id: notion_database_id ?? "",
+        },
+        properties: {
+          "Problem Number": {
+            type: "number",
+            number: parseInt(problem.questionFrontendId),
+          },
+          "Problem Name": {
+            type: "title",
+            title: [
+              {
+                type: "text",
+                text: {
+                  content: problem.title,
+                },
+              },
+            ],
+          },
+          Topics: {
+            type: "multi_select",
+            multi_select: problem.topicTags.map((tag) => {
+              return {
+                name: tag.name,
+              };
+            }),
+          },
+          Difficulty: {
+            type: "select",
+            select: {
+              name: problem.difficulty,
+              color:
+                problem.difficulty === "Easy"
+                  ? "green"
+                  : problem.difficulty === "Medium"
+                  ? "orange"
+                  : "red",
+            },
+          },
+          Link: {
+            type: "url",
+            url: `https://leetcode.com/problems/${problem.titleSlug}/`,
+          },
+          Status: {
+            type: "select",
+            select: {
+              name: "Done",
+              color: "green",
+            },
+          },
+          Date: {
+            type: "date",
+            date: {
+              start: new Date(submission.timestamp).toISOString(),
+            },
+          },
+        },
+        children: [
           {
-            type: "text",
-            text: {
-              content: problem.title,
+            object: "block",
+            type: "code",
+            code: {
+              caption: [],
+              rich_text: [
+                {
+                  type: "text",
+                  text: {
+                    content: submission_detail.code,
+                  },
+                },
+              ],
+              language: "python",
             },
           },
         ],
-      },
-      Topics: {
-        type: "multi_select",
-        multi_select: problem.topicTags.map((tag) => {
-          return {
-            name: tag.name,
-          };
-        }),
-      },
+      });
 
-      // ...
-      Difficulty: {
-        type: "select",
-        select: {
-          name: problem.difficulty,
-          color: "orange",
-        },
-      },
-      Link: {
-        type: "url",
-        url: `https://leetcode.com/problems/${problem.titleSlug}/`,
-      },
-      Status: {
-        type: "select",
-        select: {
-          name: "Done",
-          color: "green",
-        },
-      },
-      Date: {
-        type: "date",
-        date: {
-          start: new Date(parseInt(submission.timestamp) * 1000).toISOString(),
-        },
-      },
-    },
-  });
+      console.log("Page created:", response);
+    } catch (error: any) {
+      console.error("Error creating page:", error);
+      if (error.code === "rate_limited") {
+        // Retry after a delay using exponential backoff
+        const delay = Math.pow(2, error.retryAfterSeconds) * 1000;
+        console.log(`Retrying after ${delay}ms...`);
+        setTimeout(createPage, delay);
+      } else if (error.code === "validation_error") {
+        console.error("Validation Error encountered");
+        console.error(error);
+      }
+    }
+  };
+
+  await createPage();
 };
 
+/**
+ * The function updates a database in Notion with submissions from LeetCode.
+ * @param {Client} notion - The `notion` parameter is an instance of the Notion client, which is used
+ * to interact with the Notion API and perform operations on the Notion database.
+ * @param {LeetCode} lc - LeetCode API client used to fetch information about the submission.
+ * @param {Submission[]} submissions - The `submissions` parameter is an array of objects representing
+ * submissions. Each submission object should have the following properties:
+ */
 export const updateDatabaseWithSubmissions = async (
   notion: Client,
   lc: LeetCode,
-  submissions: RecentSubmission[]
+  submissions: Submission[]
 ) => {
   for (const submission of submissions) {
     await updateDatabaseWithSubmission(
@@ -97,6 +153,18 @@ export const updateDatabaseWithSubmissions = async (
   }
 };
 
+/**
+ * The function `getDatabaseEntriesByProblemNumber` retrieves database entries from a Notion database
+ * based on a specified problem number.
+ * @param {Client} notion - The `notion` parameter is an instance of the Notion client, which is used
+ * to interact with the Notion API and perform operations on the database.
+ * @param {string} database_id - The `database_id` parameter is the unique identifier of the database
+ * in Notion. It is used to specify which database to query for entries.
+ * @param {number} problem_number - The `problem_number` parameter is the number that you want to use
+ * to filter the database entries. It is used in the filter condition to find entries that have a
+ * property called "Problem Number" with a value equal to the provided `problem_number`.
+ * @returns the results of the query made to the Notion database.
+ */
 export const getDatabaseEntriesByProblemNumber = async (
   notion: Client,
   database_id: string,
@@ -114,13 +182,26 @@ export const getDatabaseEntriesByProblemNumber = async (
   return response.results;
 };
 
+/**
+ * The function removes already submitted problems from a list of submissions.
+ * @param {Client} notion - The `notion` parameter is an instance of the Notion client, which is used
+ * to interact with the Notion API and perform operations on the Notion database.
+ * @param {LeetCode} lc - The `lc` parameter is an instance of the `LeetCode` class, which is used to
+ * interact with the LeetCode API and retrieve information about problems and submissions.
+ * @param {string} database_id - The `database_id` parameter is the ID of the Notion database where the
+ * problems are stored.
+ * @param {Submission[]} submissions - An array of submission objects. Each submission object
+ * represents a problem submission and contains information such as the problem number, submission
+ * date, and submission status.
+ * @returns an array of submissions that have not already been submitted.
+ */
 export const removeAlreadySubmittedProblems = async (
   notion: Client,
   lc: LeetCode,
   database_id: string,
-  submissions: RecentSubmission[]
+  submissions: Submission[]
 ) => {
-  const new_submissions: RecentSubmission[] = [];
+  const new_submissions: Submission[] = [];
   for (const submission of submissions) {
     const entries = await getDatabaseEntriesByProblemNumber(
       notion,
